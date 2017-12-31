c095c3b90ed096ce7f47ce554d9b031264ccf7bc
David Yonwook Chung <yon_gospel@hotmail.com>
2017-12-30 19:55:55 -0800

    created drop down menu for todoMaker tags with prefilled options

--- client/public/input.js
+++ client/public/input.js
@@ -1,7 +1,13 @@
 angular.module('app')
   .component('todoMaker', {
+      controller: function() {
+        this.tagOptions = ["Important", "Work", "School", "Hobby", "Daily Routine"]
+      },
       template:`
       Insert todo:
       <input></input>
+      Select type:
+      <select ng-model="selectedTag" ng-options="tag for tag in $ctrl.tagOptions"></select>
+      Add todo!
       `
   })
