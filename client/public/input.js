b5e6e1c1bc4826636cbb68f1c6495b3ef6f5e7a8
David Yonwook Chung <yon_gospel@hotmail.com>
2017-12-30 20:02:24 -0800

    configured the  todoMaker to receive todoTitle and selected Tag in console.log

--- client/public/input.js
+++ client/public/input.js
@@ -1,13 +1,16 @@
 angular.module('app')
   .component('todoMaker', {
       controller: function() {
-        this.tagOptions = ["Important", "Work", "School", "Hobby", "Daily Routine"]
+        this.tagOptions = ["Important", "Work", "School", "Hobby", "Daily Routine"];
+        this.onSubmit = (todoTitle, selectedTag) => {
+           console.log(todoTitle, selectedTag)
+        }
       },
       template:`
       Insert todo:
-      <input></input>
+      <input ng-model="$ctrl.todo"></input>
       Select type:
-      <select ng-model="selectedTag" ng-options="tag for tag in $ctrl.tagOptions"></select>
-      Add todo!
+      <select ng-model="$ctrl.selectedTag" ng-options="tag for tag in $ctrl.tagOptions"></select>
+      <button ng-click="$ctrl.onSubmit($ctrl.todo, $ctrl.selectedTag)">Add todo</button>
       `
   })
